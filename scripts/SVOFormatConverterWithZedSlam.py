e0cd8b16de76f6bca6dc19ac845f402d70443f6d
Weiyan Zhu <weiyanz@berkeley.edu>
2020-11-17 10:36:23 -0800

    fix pose used

--- scripts/SVOFormatConverterWithZedSlam.py
+++ scripts/SVOFormatConverterWithZedSlam.py
@@ -24,13 +24,16 @@ def main():
     #initialize camera
     input_type = sl.InputType()
     input_type.set_from_svo_file(filepath)
-    init = sl.InitParameters(input_t=input_type, svo_real_time_mode=False, depth_mode= sl.DEPTH_MODE.PERFORMANCE)#, coordinate_units=sl.UNIT.METER)#, coordinate_system=sl.COORDINATE_SYSTEM.RIGHT_HANDED_Z_UP)
+    init = sl.InitParameters(input_t=input_type, svo_real_time_mode=False, depth_mode= sl.DEPTH_MODE.ULTRA, coordinate_units=sl.UNIT.METER)#, coordinate_system=sl.COORDINATE_SYSTEM.RIGHT_HANDED_Z_UP)
     cam = sl.Camera()
     status = cam.open(init)
+
     if status != sl.ERROR_CODE.SUCCESS:
         print(repr(status))
         exit()
 
+    tracking_parameters = sl.PositionalTrackingParameters()
+    err = cam.enable_positional_tracking(tracking_parameters)
     #set paths to save data
     path_prefix = Path(os.path.dirname(__file__))/"svoData"
     depth_image_folder = path_prefix/ "depth"
@@ -50,22 +53,22 @@ def main():
     rgb_image = sl.Mat()
     #go through svo files to get info
     runtime_param = sl.RuntimeParameters()
-    runtime_param.sensing_mode = sl.SENSING_MODE.FILL
+    runtime_param.sensing_mode = sl.SENSING_MODE.STANDARD
 
     total_frames = cam.get_svo_number_of_frames()
-    '''print out extrinsic values
-    trans = cam.get_camera_information(resizer=sl.Resolution(640,480)).camera_imu_transform
-    rightToLeft = cam.get_camera_information(resizer=sl.Resolution(640,480)).calibration_parameters.stereo_transform
-    rightToLeft.inverse()
-    trans.inverse()
-    trans = np.matmul(rightToLeft.m, trans.m)
-    print("sensor pose R inverse", trans)
+    '''print out extrinsic values'''
+    ImuToLeft = cam.get_camera_information(resizer=sl.Resolution(640,480)).camera_imu_transform
+    leftToRight = cam.get_camera_information(resizer=sl.Resolution(640,480)).calibration_parameters.stereo_transform
+    trans = np.matmul(ImuToLeft.m, leftToRight.m)
+    trans = np.linalg.inv(trans)
+    print("right to imu pose", trans)
+    '''
     lc = cam.get_camera_information(resizer=sl.Resolution(640,480)).calibration_parameters.left_cam
     print("intrinsic left", [lc.fx, lc.fy, lc.cx, lc.cy])
     trans = cam.get_camera_information(resizer=sl.Resolution(640,480)).camera_imu_transform
     trans.inverse()
-    print("imu to camera trans", trans)
-    '''
+    print("imu to left camera trans", trans)'''
+
     while True:  
         err = cam.grab(runtime_param)
         
@@ -73,14 +76,14 @@ def main():
             frame = cam.get_svo_position()
 
             # Retrieve depth meastures
-            cam.retrieve_measure(depth_image, sl.MEASURE.DEPTH, sl.MEM.CPU, sl.Resolution(640, 480))
+            cam.retrieve_measure(depth_image, sl.MEASURE.DEPTH)
             #cam.retrieve_image(depth_image, sl.VIEW.DEPTH) 
             cam.retrieve_image(rgb_image, sl.VIEW.LEFT)
 
             #store images
             depth_image_path = depth_image_folder / ("%s.png" % str(frame))#.zfill(5))
             rgb_image_path = rgb_image_folder / ("%s.png" % str(frame))#.zfill(5))
-            resizedDepth = (depth_image.get_data()).astype(np.uint16)#cv2.resize(depth_image.get_data().astype(np.uint16), (640, 480))
+            resizedDepth = cv2.resize((depth_image.get_data()*1000).astype(np.uint16), (640, 480))#cv2.resize(depth_image.get_data().astype(np.uint16), (640, 480))
             resizedRGB = cv2.resize(rgb_image.get_data(), (640, 480))
             cv2.imwrite(str(depth_image_path), resizedDepth)
             cv2.imwrite(str(rgb_image_path), resizedRGB)
@@ -92,6 +95,7 @@ def main():
             rgb_im.save(dst)
 
             #store pose
+            '''
             sensors_data = sl.SensorsData()
             cam.get_sensors_data(sensors_data, time_reference = sl.TIME_REFERENCE.IMAGE)
             imu = sl.IMUData()
@@ -104,20 +108,24 @@ def main():
             poseT = zed_imu_pose.get_translation().get()
             p = np.concatenate((poseR, poseT[:,None]), axis=1)
             p = np.concatenate((p, np.array([[0,0,0,1]])), axis=0)
-
             '''
-            #p = np.linalg.inv(p)
+
             pose = sl.Pose()
             print(cam.get_position(pose, sl.REFERENCE_FRAME.WORLD))
-            p = sl.Transform()
-            print(pose.pose_data(p))
-            print(p.get_translation().get())
-            '''
+            poseTransform = sl.Transform()
+            pose.pose_data(poseTransform)
+            poseRotation = poseTransform.get_rotation_matrix().r
+            poseTranslation = poseTransform.get_translation().get()
+            p = np.concatenate((poseRotation, poseTranslation[:,None]), axis=1)
+            p = np.concatenate((p, np.array([[0,0,0,1]])), axis=0)
+            print(p)
+            #p = np.linalg.inv(p)
+
             pose_path = pose_folder / ("%s.txt" % str(frame))#.zfill(5))
             pose_file = open(pose_path, 'w')
-            pose_file.write("%s %s %s %s\n" % (p[0][0], p[0][1], p[0][2], float(p[0][3])/1000))
-            pose_file.write("%s %s %s %s\n" % (p[1][0], p[1][1], p[1][2], float(p[1][3])/1000))
-            pose_file.write("%s %s %s %s\n" % (p[2][0], p[2][1], p[2][2], float(p[2][3])/1000))
+            pose_file.write("%s %s %s %s\n" % (p[0][0], p[0][1], p[0][2], p[0][3]))#(p[0][3])/1000))
+            pose_file.write("%s %s %s %s\n" % (p[1][0], p[1][1], p[1][2], p[1][3]))#float(p[1][3])/1000))
+            pose_file.write("%s %s %s %s\n" % (p[2][0], p[2][1], p[2][2], p[2][3]))#float(p[2][3])/1000))
             pose_file.write("%s %s %s %s\n" % (p[3][0], p[3][1], p[3][2], p[3][3]))
 
         # Display progress
